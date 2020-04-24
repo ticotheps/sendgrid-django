4d60a9d8219bfa922199e901c95db8af626716c8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-24 01:40:58 -0400

    Configures '/hello_sendgrid/hello_sendgrid/urls.py' file

--- hello_sendgrid/hello_sendgrid/urls.py
+++ hello_sendgrid/hello_sendgrid/urls.py
@@ -13,9 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#-------------------------------------------------------------
+
+# # Newer code from django v3.0.5
+# from django.contrib import admin
+# from django.urls import path
+
+# from hello_sendgrid import views
+
+# # Newer code from django v3.0.5
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('sendgrid/', index, name='sendgrid'),
+# ]
+
+#-------------------------------------------------------------
+
+# Older code from 04/05/19
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+
+from .views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^sendgrid/', index, name='sendgrid'),
 ]
+
+
